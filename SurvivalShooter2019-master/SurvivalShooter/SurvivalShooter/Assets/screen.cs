4b24facf9e21375e5ac3cc57d1d777e70ebb2ce0
astora83 <62930880+astora83@users.noreply.github.com>
2020-11-06 16:46:43 -0600

    almost

---
+++ SurvivalShooter2019-master/SurvivalShooter/SurvivalShooter/Assets/screen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera cam1;
+    public Camera cam2;
+    void Start()
+    {
+        cam1.rect = new Rect(0, 0, 0.5f, 1);
+        cam2.rect = new Rect(0.5f, 0, 0.5f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
